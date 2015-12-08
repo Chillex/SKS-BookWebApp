e1692f4fcfbb5bc04afb17cacb6bd63c923092f3
Alexander Müller <if14b069@technikum-wien.at>
2015-12-08 21:22:41 +0100

    modified to use correct user roles

--- src/at/technikum/BookWebService.java
+++ src/at/technikum/BookWebService.java
@@ -2,12 +2,16 @@ package at.technikum;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface BookWebService {
+	@WebMethod
 	public List<Book> getAllBooks();
+	@WebMethod
 	public List<Book> getBooksByTitle(@WebParam(name = "title") String title);
+	@WebMethod
 	public void addBooks(@WebParam(name = "book") List<Book> books);
 }
